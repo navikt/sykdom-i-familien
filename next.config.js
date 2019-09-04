b6ff6d6268cf16e7f51dd1ec14399a0ff19e7b93
Frode Hansen <frode.hansen@nav.no>
2019-09-04 15:07:44 +0200

    Import bug

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-module.exports = {
+const withLess = require('@zeit/next-less');
+
+module.exports = withLess({
+  lessLoaderOptions: {
+    javascriptEnabled: true
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader'
     });
-
     return config;
   }
-};
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,19 @@ import Layout from '../components/Layout';
 import Markdown from 'react-markdown';
 import matter from 'gray-matter';
 import toc from 'remark-toc';
+import Panel from 'nav-frontend-paneler';
+
+import '../styles.less';
 
 const content = require('../content/intro.md').default;
 const parsed = matter(content);
 
-interface WhoaProps {}
+interface Props {}
 
-const Whoa: React.FunctionComponent<WhoaProps> = (props) => (
+const Whoa: React.FunctionComponent<Props> = (props) => (
   <Layout>
     <h2>{parsed.data.title}</h2>
+    <Panel>sdf</Panel>
     <hr />
     <Markdown source={parsed.content} plugins={[toc]} />
   </Layout>
