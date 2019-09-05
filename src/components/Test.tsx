195a9cc007442c4411a54c6e381f7ea17fac58b0
Frode Hansen <frode.hansen@nav.no>
2019-09-05 09:34:44 +0200

    Plugins test

---
+++ src/components/Test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+interface TestProps {}
+
+const Test: React.FunctionComponent<TestProps> = (props) => <div>TestKomponnet</div>;
+
+export default Test;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,67 @@ import Layout from '../components/Layout';
 import Markdown from 'react-markdown';
 import matter from 'gray-matter';
 import toc from 'remark-toc';
+import MarkIt from 'markdown-to-jsx';
+import Test from '../components/Test';
+import visit from 'unist-util-visit';
 
-const content = require('../content/intro.md').default;
+const content = require('../content/pleiepenger/kortFortalt.md').default;
 const parsed = matter(content);
 
+const renderers = {
+  // text: () => <span>Data</span>,
+  DatePicker: () => <span>Date</span>
+};
+
 interface WhoaProps {}
+
+function linker() {
+  function transformer(tree) {
+    console.log(tree);
+    visit(tree, 'inlineCode', function(node, index, parent) {
+      const value = typeof node.value === 'string' ? (node.value as string) : '';
+      console.log(node.type);
+      if (parent.type !== 'link' && /nav [a-z-.]+/.test(node.value as string)) {
+        console.log('match');
+
+        parent.children[index] = {
+          type: 'link',
+          url: 'https://nav.no/' + value.split(' ')[1],
+          children: [
+            {
+              type: node.type,
+              value: 'abcv',
+              position: node.position
+            }
+          ],
+          position: node.position
+        };
+      }
+    });
+
+    return tree;
+  }
+
+  return transformer;
+}
 
 const Whoa: React.FunctionComponent<WhoaProps> = (props) => (
   <Layout>
     <h2>{parsed.data.title}</h2>
     <hr />
-    <Markdown source={parsed.content} plugins={[toc]} />
+    <Markdown source={parsed.content} astPlugins={[linker()]} includeNodeIndex={true} renderers={renderers} />
+    <MarkIt
+      children={parsed.content}
+      options={{
+        overrides: {
+          DatePicker: {
+            component: Test
+          }
+        }
+      }}
+    />
+
+    <hr />
   </Layout>
 );
 
