3d391da0823a3e9a3454996dbc726eaa1b7624d6
Frode Hansen <frodehansen@Frodes-MacBook-Pro.local>
2019-11-05 13:36:56 +0100

    Autodeploy test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -93,7 +93,6 @@ const Hovedside: React.FunctionComponent<Props> = ({ data, intl }: Props & Injec
                                 <SanityBlockContent content={story.description} />
                             </LinkPanel>
                         ))}
-                    <hr style={{ display: 'none' }} />
                 </FrontpagePanelWrapper>
             </Box>
         </Frontpage>
--- src/utils/inltUtils.ts
+++ src/utils/inltUtils.ts
@@ -4,8 +4,6 @@ import { Locale, defaultLocale } from '../i18n/locale';
 const intlHelper = (intl: InjectedIntl, id: string, value?: { [key: string]: MessageValue }): string =>
     intl.formatMessage({ id }, value);
 
-/** Get locale content */
 export const getLocale = (intl: InjectedIntl): Locale => (intl ? (intl.locale as Locale) : defaultLocale);
 
-// Another comment
 export default intlHelper;
