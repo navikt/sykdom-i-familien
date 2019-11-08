9f3f52f85ad1ae91b01335675fbb48d9d6eaf721
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2019-11-08 12:02:15 +0100

    refactors out dataset to environment. Resolves env with script

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
             resolve: 'gatsby-source-sanity',
             options: {
                 projectId: '8ux9tyb9',
-                dataset: 'staging',
+                dataset: `${process.env.DATASET_ENV ? process.env.DATASET_ENV : 'staging'}`,
                 token: `${process.env.SANITY_TOKEN}`,
                 watchMode: true,
                 overlayDrafts: true
---
+++ scripts/resolve-dataset-environment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#set -x
+
+case $GITHUB_REF in
+
+    'refs/heads/dev-*')
+        export DATASET_ENV=staging
+        echo "Exported DATASET_ENV=staging"
+        ;;
+
+    'refs/heads/master')
+        export DATASET_ENV=production
+        echo "Exported DATASET_ENV=production"
+        ;;
+esac
