6c219c0ddfcba6c26ad1e3f29d0de433b4b32b32
Frode Hansen <frodehansen@Frodes-MacBook-Pro.local>
2019-11-13 16:00:22 +0100

    Mobile rasmus

--- src/components/veileder/rasmus/RasmusVeileder.tsx
+++ src/components/veileder/rasmus/RasmusVeileder.tsx
@@ -3,6 +3,7 @@ import Veilederpanel from 'nav-frontend-veilederpanel';
 import RasmusSirkelSVG from './RasmusSirkelSVG';
 
 import './rasmusVeileder.less';
+import MediaQuery from 'react-responsive';
 
 interface Props {
     kompakt?: boolean;
@@ -12,9 +13,16 @@ interface Props {
 
 const RasmusVeileder: React.FunctionComponent<Props> = ({ type, children, kompakt }) => (
     <div className={'veilederpanel--rasmus'}>
-        <Veilederpanel svg={<RasmusSirkelSVG />} type={type} kompakt={kompakt}>
-            {children}
-        </Veilederpanel>
+        <MediaQuery maxWidth={800}>
+            <Veilederpanel svg={<RasmusSirkelSVG />} type={'plakat'} kompakt={kompakt}>
+                {children}
+            </Veilederpanel>
+        </MediaQuery>
+        <MediaQuery minWidth={801}>
+            <Veilederpanel svg={<RasmusSirkelSVG />} type={type} kompakt={kompakt}>
+                {children}
+            </Veilederpanel>
+        </MediaQuery>
     </div>
 );
 
