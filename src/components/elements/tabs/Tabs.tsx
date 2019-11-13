797c2875a8f41399b14ce5a43396be16afcad7ef
Frode Hansen <frodehansen@Frodes-MacBook-Pro.local>
2019-11-13 16:57:28 +0100

    Mobile tabs

--- src/components/elements/tabs/Tabs.tsx
+++ src/components/elements/tabs/Tabs.tsx
@@ -7,6 +7,7 @@ import './tabs.less';
 import Select from './select/Select';
 import { Undertittel } from 'nav-frontend-typografi';
 import { BlockContentObjectTypes } from '../../../sanity/types/objects';
+import MediaQuery from 'react-responsive';
 
 export interface Tab {
     index: number;
@@ -26,44 +27,49 @@ const bem = bemUtils('tabs');
 const Tabs: React.FunctionComponent<TabsProps> = ({ tabs, presentation, title }: TabsProps) => {
     const [selectedTab, selectTab] = useState({ index: 0 });
 
+    const renderTabs = () => (
+        <>
+            {title && (
+                <Undertittel tag="h3" className={bem.element('title')}>
+                    {title}
+                </Undertittel>
+            )}
+
+            <div role="tablist" className={bem.element('tabs')}>
+                {tabs.map((tab) => (
+                    <TabButton
+                        key={tab.index}
+                        label={tab.label}
+                        icon={tab.illustration}
+                        onSelect={() => selectTab({ index: tab.index })}
+                        isSelected={selectedTab.index === tab.index}
+                    />
+                ))}
+            </div>
+        </>
+    );
+
+    const renderSelect = () => (
+        <>
+            {title && (
+                <Undertittel tag="h3" className={bem.element('title')}>
+                    {title}
+                </Undertittel>
+            )}
+            <div className={bem.element('select')}>
+                <Select
+                    choices={tabs}
+                    onChoiceSelect={(index) => selectTab({ index })}
+                    selected={tabs[selectedTab.index]}
+                />
+            </div>
+        </>
+    );
+
     return (
         <div className={bem.block}>
-            {presentation === 'tabs' ? (
-                <>
-                    {title && (
-                        <Undertittel tag="h3" className={bem.element('title')}>
-                            {title}
-                        </Undertittel>
-                    )}
-
-                    <div role="tablist" className={bem.element('tabs')}>
-                        {tabs.map((tab) => (
-                            <TabButton
-                                key={tab.index}
-                                label={tab.label}
-                                icon={tab.illustration}
-                                onSelect={() => selectTab({ index: tab.index })}
-                                isSelected={selectedTab.index === tab.index}
-                            />
-                        ))}
-                    </div>
-                </>
-            ) : (
-                <>
-                    {title && (
-                        <Undertittel tag="h3" className={bem.element('title')}>
-                            {title}
-                        </Undertittel>
-                    )}
-                    <div className={bem.element('select')}>
-                        <Select
-                            choices={tabs}
-                            onChoiceSelect={(index) => selectTab({ index })}
-                            selected={tabs[selectedTab.index]}
-                        />
-                    </div>
-                </>
-            )}
+            <MediaQuery maxWidth={800}>{renderSelect()}</MediaQuery>
+            <MediaQuery minWidth={801}>{presentation === 'tabs' ? renderTabs() : renderSelect()}</MediaQuery>
             {tabs.map((tab) => (
                 <TabPanel key={tab.index} tab={tab} selected={tab.index === selectedTab.index} />
             ))}
