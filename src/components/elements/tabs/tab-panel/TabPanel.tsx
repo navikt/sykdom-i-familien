24d0bf08aa5c3ef6a3cdf7690c7ef455e11dfc3f
Frode Hansen <frodehansen@Frodes-MacBook-Pro.local>
2019-11-14 10:04:22 +0100

    Always show tab title

--- src/components/elements/tabs/tab-panel/TabPanel.tsx
+++ src/components/elements/tabs/tab-panel/TabPanel.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Tab } from '../Tabs';
 import SanityBlockContent from '../../../../sanity/components/sanity-block-content/SanityBlockContent';
 import bemUtils from '../../../../utils/bemUtils';
-import { Systemtittel } from 'nav-frontend-typografi';
+import { Undertittel } from 'nav-frontend-typografi';
 
 interface Props {
     tab: Tab;
@@ -18,9 +18,9 @@ const TabPanel: React.FunctionComponent<Props> = ({ tab, selected, bgcolor }) =>
         key={tab.label}
         className={bem.element('panel', selected ? 'selected' : 'hidden')}
         style={bgcolor ? { backgroundColor: bgcolor } : undefined}>
-        <Systemtittel tag="h3" className={bem.element('panelTitle')}>
-            {tab.label}
-        </Systemtittel>
+        <Undertittel tag="h3" className={bem.element('panelTitle')}>
+            {tab.contentTitle || tab.label}
+        </Undertittel>
         <SanityBlockContent content={tab.content} />
     </div>
 );
