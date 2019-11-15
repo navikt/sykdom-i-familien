1b6bb254857c040cee26fc93f7453c246a851a77
Frode Hansen <frodehansen@Frodes-MacBook-Pro.local>
2019-11-15 12:49:33 +0100

    Hotjar update

--- plugins/nav-decorator/gatsby-ssr.js
+++ plugins/nav-decorator/gatsby-ssr.js
@@ -6,6 +6,22 @@ export const onRenderBody = ({ setHeadComponents, setPreBodyComponents, setPostB
     <head key="nav-styles" dangerouslySetInnerHTML={{ __html: decoratorFragments.NAV_STYLES }} />,
     <head key="nav-scripts" dangerouslySetInnerHTML={{ __html: decoratorFragments.NAV_SCRIPTS }} />,
     <head
+      key="hotjar"
+      dangerouslySetInnerHTML={{
+        __html: `
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:148751,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>`
+      }}
+    />,
+    <head
       key="nav-resources"
       dangerouslySetInnerHTML={{
         __html: decoratorFragments.NAV_MENU_RESOURCES
