a482dfccc982c33046276e5f6538048c94417b1a
Frode Hansen <frodehansen@Frodes-MacBook-Pro.local>
2019-12-09 18:36:12 +0100

    button style on blockContent

--- src/sanity/components/sanity-block/SanityBlock.tsx
+++ src/sanity/components/sanity-block/SanityBlock.tsx
@@ -3,6 +3,8 @@ import BlockContent from '@sanity/block-content-to-react';
 import Lenke from 'nav-frontend-lenker';
 import { BlockContentType } from '../../types/parts';
 
+import './sanityBlock.less';
+
 interface Props {
     content: BlockContentType;
 }
@@ -11,6 +13,12 @@ const ListItemRenderer = (props: { children: React.ReactNode; node: { style: str
     return <li className={props.node.style}>{props.children}</li>;
 };
 
+const BlockRenderer = (props: { children: React.ReactNode; node: { style: string } }) => {
+    if (props.node.style === 'button') {
+        return <div className="sanityLinkButtonWrapper">{props.children}</div>;
+    }
+    return <p>{props.children}</p>;
+};
 const LinkRenderer = (props: {
     children: React.ReactNode;
     mark: {
@@ -25,16 +33,18 @@ const LinkRenderer = (props: {
     );
 };
 
-const SanityBlock: React.FunctionComponent<Props> = ({ content }) => (
-    <BlockContent
-        blocks={content}
-        serializers={{
-            listItem: ListItemRenderer,
-            marks: {
-                link: LinkRenderer
-            }
-        }}
-    />
-);
-
+const SanityBlock: React.FunctionComponent<Props> = ({ content }) => {
+    return (
+        <BlockContent
+            blocks={content}
+            serializers={{
+                listItem: ListItemRenderer,
+                marks: {
+                    link: LinkRenderer
+                },
+                block: BlockRenderer
+            }}
+        />
+    );
+};
 export default SanityBlock;
