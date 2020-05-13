8d0811c59199a9ca8ad53829d0972a13e1e1acf7
Frode Hansen <frode.hansen@bekk.no>
2020-05-13 12:19:55 +0200

    revert dev dekoratøer

--- plugins/nav-decorator/getDecorator.js
+++ plugins/nav-decorator/getDecorator.js
@@ -5,7 +5,7 @@ const { JSDOM } = jsdom;
 const requestDecorator = (callback) => {
   const url =
     process.env.NAIS_CLUSTER_NAME === 'dev-sbs'
-      ? 'https://appres-q0.nav.no/common-html/v4/navno/'
+      ? 'https://www-q0.nav.no/person/nav-dekoratoren/'
       : 'https://appres.nav.no/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer-withmenu=true';
   return request(url, callback);
 };
