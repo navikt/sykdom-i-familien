928a10850e88ae5c6d529436f6792bbca9c5f981
Andre Wolden <wolden.andre@gmail.com>
2020-10-06 13:23:29 +0200

    legger til custom-component for inngang til omsorgsdagerkalkulatoren

---
+++ src/custom-components/omsorgsdager-kalkulator-inngang/OmsorgsdagerKalkulatorInngang.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Panel from 'nav-frontend-paneler';
+import SvgCalculatorLogo from './SvgCalculatorLogo';
+import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
+import bemUtils from '../../utils/bemUtils';
+import './omsorgsdager-kalkulator-inngang.less';
+import Lenke from 'nav-frontend-lenker';
+
+const bem = bemUtils('omsorgsdager-kalkulator-inngang');
+
+const OmsorgsdagerKalkulatorInngang = () => (
+    <Panel border={true} className={bem.block}>
+        <SvgCalculatorLogo />
+        <Undertittel className={bem.element('calcUndertittel')}>Kalkulator omsorgsdager</Undertittel>
+        <Normaltekst>Med denne kalkulatoren kan du regne ut hvor mange omsorgsdager du kan ha rett på.</Normaltekst>
+        <Lenke href={"https://www.nav.no/familie/sykdom-i-familien/nb/omsorgsdagerkalkulator-info"}>Fortsett til kalkulator</Lenke>
+    </Panel>
+);
+
+export default OmsorgsdagerKalkulatorInngang;
